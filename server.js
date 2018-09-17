f6e077a81d6a05cd8e8e706b5ba18eda3b9fc24d
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-17 12:55:47 -0700

    updated mongo path

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/l5rscrapedb");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
- var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/l5rscrapedb";
+ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // // Set mongoose to leverage built in JavaScript ES6 Promises
 // // Connect to the Mongo DB

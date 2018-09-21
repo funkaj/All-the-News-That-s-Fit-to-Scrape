4c3d080e060a770c99ae2f51d3ff1772c65a4f28
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-21 14:21:29 -0700

    updated some stylying. added post for saving article, and get for retriving saved articles. They still need to work

--- routes/routes.js
+++ routes/routes.js
@@ -104,4 +104,66 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
+    // Route for grabbing a specific saved Articles
+    app.get("/saved-articles", function (req, res) {
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findAll({})
+           // Now, we grab every h2 within an article tag, and do the following:
+           $("h1").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+        });
+
+    });
+    // Route for saving/updating an Article
+    app.post("/save-articles/:id", function (req, res) {
+        // Create a new article and pass the req.body to the entry
+        db.saveArticle.create(req.body)
+            .then(function (dbsaveArticle) {
+                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+                return db.Article.findOneAndUpdate({
+                    _id: req.params.id
+                }, 
+                {
+                    title: dbsaveArticle.title
+                },
+                {
+                    title: dbsaveArticle.link
+                },
+                {
+                    note: dbsaveArticle._id
+                }, {
+                    new: true
+                });
+            })
+            .then(function (dbArticle) {
+                // If we were able to successfully update an Article, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
 }

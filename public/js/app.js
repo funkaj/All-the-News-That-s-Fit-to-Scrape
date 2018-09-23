e4090ff76d615ff48fbc2af888ed52d4e1d37c4f
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-23 03:07:13 -0700

    updated saved layout to prepend.

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,11 @@ $(document).on("click", "#scrapeSite", function () {
   $('#articles').empty();
   $.getJSON("/articles", function (data) {
 
-      // For each one
-      data.map(function (el) {
-        
-        // Display the apropos information on the page
-        $('#articles').append(`
+    // For each one
+    data.map(function (el) {
+
+      // Display the apropos information on the page
+      $('#articles').append(`
             <div id=${el._id} class='card'>
               <div class='card-image'>
                 <img src='https://images-cdn.fantasyflightgames.com/filer_public/09/c4/09c4b5a2-b3aa-45c8-b13a-299124c8c946/l5c15_preview7.jpg'>
@@ -23,8 +23,8 @@ $(document).on("click", "#scrapeSite", function () {
               <a class="save waves-effect waves-light btn-large" data-id=${el._id} style='margin: 10px;'>Save Article</a>
             </div>
         `)
-        
-      })
+
+    })
   })
 })
 // Grab the saved articles as a json
@@ -35,11 +35,11 @@ $(document).on('click', '#saved-articles', function () {
     method: 'GET',
     url: '/saved'
   }).then(function (data) {
- 
+
     data.map(function (el) {
-     
+
       // Display the apropos information on the page
-      $('#articles').append(`
+      $('#articles').prepend(`
           <div id=${el._id} class='card'>
             <div class='card-image'>
               <img src='https://images-cdn.fantasyflightgames.com/filer_public/09/c4/09c4b5a2-b3aa-45c8-b13a-299124c8c946/l5c15_preview7.jpg'>
@@ -52,10 +52,10 @@ $(document).on('click', '#saved-articles', function () {
             <div class='card-action'>
               <a href='https://www.fantasyflightgames.com${el.link}' 'data-id=${el._id}'>Read Me</a>
             </div>
-            <a class="waves-effect waves-light btn-large" data-id=${el._id} style='margin: 10px;'>Notes</a>
+            <a class="note waves-effect waves-light btn-large" data-id=${el._id} style='margin: 10px;'>Notes</a>
           </div>
       `)
-      
+
     })
   })
 })
@@ -92,38 +92,37 @@ $(document).on("click", ".save", function () {
 
 })
 // Whenever someone clicks a p tag
-// $(document).on("click", "p", function() {
-//   // Empty the notes from the note section
-//   $("#notes").empty();
-//   // Save the id from the p tag
-//   var thisId = $(this).attr("data-id");
-
-//   // Now make an ajax call for the Article
-//   $.ajax({
-//     method: "GET",
-//     url: "/articles/" + thisId
-//   })
-//     // With that done, add the note information to the page
-//     .then(function(data) {
-//       console.log(data);
-//       // The title of the article
-//       $("#notes").append("<h2>" + data.title + "</h2>");
-//       // An input to enter a new title
-//       $("#notes").append("<input id='titleinput' name='title' >");
-//       // A textarea to add a new note body
-//       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-//       // A button to submit a new note, with the id of the article saved to it
-//       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-//       // If there's a note in the article
-//       if (data.note) {
-//         // Place the title of the note in the title input
-//         $("#titleinput").val(data.note.title);
-//         // Place the body of the note in the body textarea
-//         $("#bodyinput").val(data.note.body);
-//       }
-//     });
-// });
+$(document).on("click", ".note", function () {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id")
+  console.log(thisId)
+  // Now make an ajax call for the Article
+  $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>")
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
 
 // When you click the savenote button
 $(document).on("click", "#savenote", function () {
